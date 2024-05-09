eadfc03dbac058c5da140bf73664ab136acc6fb1
hare <rehare1223@gmail.com>
2024-05-09 21:31:00 +0900

    Change directory

--- STG/Assets/Enemy/Enemy.cs
+++ STG/Assets/Enemy/Enemy.cs
@@ -7,10 +7,19 @@ public class Enemy : MonoBehaviour
 
     Rigidbody2D rb;
 
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        Vector2 moveVec = new Vector2(-speed, 0f);
+        rb.MovePosition(rb.position + moveVec * Time.fixedDeltaTime);
     }
 
     void OnCollisionEnter2D(Collision2D other)
