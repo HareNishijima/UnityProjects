68665711ced44ff0354faf6b651119d4ffa83993
hare <rehare1223@gmail.com>
2024-05-09 21:11:32 +0900

    Add enemy

---
+++ STG/Assets/Enemy/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Shot")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
