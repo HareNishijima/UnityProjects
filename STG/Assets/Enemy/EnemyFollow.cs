7438270d26c1da67d8202963e650a9034859eef9
hare <rehare1223@gmail.com>
2024-05-13 20:36:40 +0900

    Add enemy follow

--- STG/Assets/Enemy/EnemyFollow.cs
+++ STG/Assets/Enemy/EnemyFollow.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour
 {
+
+    public EnemyLeader enemyLeader;
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        enemyLeader = GetComponent<EnemyLeader>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        rb.MovePosition(rb.position);
+    }
 
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Shot")
+        {
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
     }
 }
