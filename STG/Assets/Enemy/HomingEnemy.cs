178826bfe15a759e4f37273c59e07912e2bd78c1
hare <rehare1223@gmail.com>
2024-05-27 22:25:29 +0900

    HomingEnemy追加

---
+++ STG/Assets/Enemy/HomingEnemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingEnemy : MonoBehaviour
+{
+
+    GameObject player;
+
+    public float maxHomingAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
