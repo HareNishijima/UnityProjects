133407818c2bb287d2f3fd5bb1296f6e19dc0a6b
hare <rehare1223@gmail.com>
2024-05-27 22:34:03 +0900

    HomingEnemy前進

--- STG/Assets/Enemy/HomingEnemy.cs
+++ STG/Assets/Enemy/HomingEnemy.cs
@@ -6,18 +6,21 @@ public class HomingEnemy : MonoBehaviour
 {
 
     GameObject player;
+    Rigidbody2D rb;
 
     public float maxHomingAngle;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
+        Vector2 moveVec = new Vector2(transform.right.x, transform.right.y) * -1f;
+        rb.MovePosition(rb.position + moveVec * speed * Time.deltaTime);
     }
 }
