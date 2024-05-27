b697382cde36aa4e575a435ada858f99aa4869e8
hare <rehare1223@gmail.com>
2024-05-27 22:08:28 +0900

    スクリプト名変更

--- STG/Assets/Enemy/MirrorZMoveEnemyGuide.cs
+++ STG/Assets/Enemy/MirrorZMoveEnemyGuide.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using System.Linq;
 using System;
 
-public class EnemyGuide : MonoBehaviour
+public class MirrorZMoveEnemyGuide : MonoBehaviour
 {
     enum State { FirstForward, Back, LastForward, Stop }
     State state;
