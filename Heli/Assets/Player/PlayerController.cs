cee29e0101f4bff802b9d00c492504aa2c300e21
HareNishijima <rehare1223@gmail.com>
2024-07-28 22:40:39 +0900

    移動により傾く

--- Heli/Assets/Player/PlayerController.cs
+++ Heli/Assets/Player/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
 
     Vector2 position;
+    Quaternion quaternion;
     Rigidbody2D rigidbody2d;
 
     public float speed;
@@ -14,17 +15,23 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         position = new Vector2(0f,0f);
+        quaternion = Quaternion.identity;
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     void Update(){
         Vector2 input = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
         position += input * speed * Time.deltaTime;
+
+        float rotate = transform.rotation.z;
+        rotate = Input.GetAxis("Vertical") - Input.GetAxis("Horizontal");
+        quaternion = Quaternion.AngleAxis(rotate * 45f, Vector3.forward);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         rigidbody2d.position = position;
+        transform.rotation = quaternion;
     }
 }
