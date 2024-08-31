575d0331f916c5c850a4bb009ad9bbfb4e02b8b4
HareNishijima <rehare1223@gmail.com>
2024-08-31 22:12:55 +0900

    リファクタリング

--- Heli/Assets/Player/PlayerController.cs
+++ Heli/Assets/Player/PlayerController.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEditor;
 using UnityEngine;
 
@@ -21,32 +20,31 @@ public class PlayerController
 
     public Quaternion NewQuaternion(Vector2 rawInput, Quaternion current)
     {
-        // 1秒でプレイヤーが180度回転するスピード
-        float angleSpeed = 180f;
+        // プレイヤーの角度スピード(1秒間に270度回転するスピード)
+        float deltaAngle = 270f * Time.fixedDeltaTime;
 
-        // angleZ = -180 ~ 180
+        // プレイヤーの角度を取得し、0 ~ 360から-180 ~ 180に変換
         float angleZ = current.eulerAngles.z;
         if (angleZ > 180f) angleZ -= 360f;
 
+        // キー入力で角度を変更
         float newAngleZ = 0f;
-        if (rawInput.x == 0f && rawInput.y == 0f)
-        {
-            // キーを入力していないときは0度に近づく
-            newAngleZ = -Mathf.Sign(angleZ) * angleSpeed * Time.fixedDeltaTime + angleZ;
-            if (Mathf.Sign(angleZ) != Mathf.Sign(newAngleZ)) newAngleZ = 0f;
-        }
-        else if (rawInput.y != 0f)
+        if (rawInput.y != 0f)
         {
             // 上方向への入力で角度が増加、下方向の入力で角度が減少する
-            newAngleZ = Mathf.Sign(rawInput.y) * angleSpeed * Time.fixedDeltaTime + angleZ;
+            newAngleZ = Mathf.MoveTowards(angleZ, Math.Sign(rawInput.y) * 45f, deltaAngle);
         }
-        else if (rawInput.y == 0f)
+        else if (rawInput.x != 0f && rawInput.y == 0f)
         {
             // 上下方向へ入力していないときに限り、右方向の入力で角度が減少、左方向の入力で角度が増加する
-            newAngleZ = -Mathf.Sign(rawInput.x) * angleSpeed * Time.fixedDeltaTime + angleZ;
+            newAngleZ = Mathf.MoveTowards(angleZ, Math.Sign(rawInput.x) * -45f, deltaAngle);
+        }
+        else if (rawInput.x == 0f && rawInput.y == 0f)
+        {
+            // 上下左右方向に入力していなければ、未操作時は0度に近づく
+            newAngleZ = Mathf.MoveTowards(angleZ, 0f, deltaAngle);
         }
 
-        newAngleZ = Mathf.Clamp(newAngleZ, -45f, 45f);
         return Quaternion.Euler(0f, 0f, newAngleZ);
     }
 }
