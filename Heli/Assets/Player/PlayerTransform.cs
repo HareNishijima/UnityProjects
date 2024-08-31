19cc5a5158743572eaf7d04e8d8103a53a45f838
HareNishijima <rehare1223@gmail.com>
2024-08-31 13:40:13 +0900

    キーの入力でプレイヤーを回転させる

--- Heli/Assets/Player/PlayerTransform.cs
+++ Heli/Assets/Player/PlayerTransform.cs
@@ -1,23 +1,39 @@
 using UnityEngine;
 
-class PlayerTransform {
+class PlayerTransform
+{
     public Vector2 position;
     public Quaternion quaternion;
-    float speed= 10f;
+    float speed = 10f;
 
-    public PlayerTransform(Vector2 p, Quaternion q){
+    public PlayerTransform(Vector2 p, Quaternion q)
+    {
         position = p;
         quaternion = q;
     }
 
-    public PlayerTransform Controller(Vector2 input){
+    public PlayerTransform Controller(Vector2 input)
+    {
         // 入力値から移動するベクトルを計算し、新しい座標を更新
         Vector2 moveVec = input * speed;
         Vector2 newPosition = moveVec * Time.deltaTime + position;
 
-        float r = -input.x;
-        if(r == 0f) r = input.y;
-        Quaternion qua = Quaternion.Euler(0f, 0f, r * 45f);
+        float angleZ = quaternion.eulerAngles.z;
+        float newAngleZ = angleZ;
+        if (input.x != 0f)
+        {
+            newAngleZ += -input.x;
+        }
+        else if (input.y != 0f)
+        {
+            newAngleZ += input.y;
+        }
+        else
+        {
+            // newAngleZが徐々に0に近づく
+        }
+        Debug.Log(newAngleZ);
+        Quaternion qua = Quaternion.Euler(0f, 0f, newAngleZ);
 
         return new PlayerTransform(newPosition, qua);
     }
