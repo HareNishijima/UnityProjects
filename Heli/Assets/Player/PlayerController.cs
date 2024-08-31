cfe1c9b414376415f74534b9a728139cf2dfdafd
HareNishijima <rehare1223@gmail.com>
2024-08-31 21:47:30 +0900

    wip プレイヤー傾き

--- Heli/Assets/Player/PlayerController.cs
+++ Heli/Assets/Player/PlayerController.cs
@@ -21,6 +21,25 @@ public class PlayerController
 
     public Quaternion NewQuaternion(Vector2 rawInput, Quaternion current)
     {
-        return Quaternion.Euler(0f, 0f, 0f);
+        // 1秒でプレイヤーが180度回転するスピード
+        float angleSpeed = 180f;
+
+        // angleZ = -180 ~ 180
+        float angleZ = current.eulerAngles.z;
+        if (angleZ > 180f) angleZ -= 360f;
+
+        float newAngleZ = 0f;
+        if (rawInput.x == 0f && rawInput.y == 0f)
+        {
+            newAngleZ = -Mathf.Sign(angleZ) * angleSpeed * Time.fixedDeltaTime + angleZ;
+            if (Mathf.Sign(angleZ) != Mathf.Sign(newAngleZ)) newAngleZ = 0f;
+        }
+        else
+        {
+            newAngleZ = Mathf.Sign(rawInput.y) * angleSpeed * Time.fixedDeltaTime + angleZ;
+        }
+
+        newAngleZ = Mathf.Clamp(newAngleZ, -45f, 45f);
+        return Quaternion.Euler(0f, 0f, newAngleZ);
     }
 }
