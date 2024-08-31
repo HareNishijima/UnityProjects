73c551e522c6ea0087750c2ae8d35e29b04dd576
HareNishijima <rehare1223@gmail.com>
2024-08-31 21:58:54 +0900

    プレイヤー傾き

--- Heli/Assets/Player/PlayerController.cs
+++ Heli/Assets/Player/PlayerController.cs
@@ -31,12 +31,19 @@ public class PlayerController
         float newAngleZ = 0f;
         if (rawInput.x == 0f && rawInput.y == 0f)
         {
+            // キーを入力していないときは0度に近づく
             newAngleZ = -Mathf.Sign(angleZ) * angleSpeed * Time.fixedDeltaTime + angleZ;
             if (Mathf.Sign(angleZ) != Mathf.Sign(newAngleZ)) newAngleZ = 0f;
         }
-        else
+        else if (rawInput.y != 0f)
         {
+            // 上方向への入力で角度が増加、下方向の入力で角度が減少する
             newAngleZ = Mathf.Sign(rawInput.y) * angleSpeed * Time.fixedDeltaTime + angleZ;
+        }
+        else if (rawInput.y == 0f)
+        {
+            // 上下方向へ入力していないときに限り、右方向の入力で角度が減少、左方向の入力で角度が増加する
+            newAngleZ = -Mathf.Sign(rawInput.x) * angleSpeed * Time.fixedDeltaTime + angleZ;
         }
 
         newAngleZ = Mathf.Clamp(newAngleZ, -45f, 45f);
