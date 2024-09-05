dfc7227c123951881e980c8d6693c560d13e1dc5
HareNishijima <rehare1223@gmail.com>
2024-09-05 22:25:42 +0900

    敵破壊時にパーティクル再生

--- Heli/Assets/Enemy/EnemyHP.cs
+++ Heli/Assets/Enemy/EnemyHP.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Cci;
 using UnityEngine;
 
 public class EnemyHP : MonoBehaviour
 {
     public int hp = 1;
+    public GameObject particleObject;
 
     public void Damage(int damage)
     {
         hp = hp - damage;
         if (hp <= 0)
         {
+            Instantiate(particleObject, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
