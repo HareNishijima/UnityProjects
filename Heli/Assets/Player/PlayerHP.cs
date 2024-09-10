ee77558205d015ec133277f5b36341da940345d9
HareNishijima <rehare1223@gmail.com>
2024-09-10 23:47:25 +0900

    Playerダメージスクリプト追加

---
+++ Heli/Assets/Player/PlayerHP.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Cci;
+using UnityEngine;
+
+public class PlayerHP : MonoBehaviour
+{
+    public int hp = 1;
+    public GameObject particleObject;
+
+    public void Damage(int damage)
+    {
+        hp = hp - damage;
+        Instantiate(particleObject, transform.position, Quaternion.identity);
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
