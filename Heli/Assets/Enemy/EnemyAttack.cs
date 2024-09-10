2cb20ee9ae0d9aba2bae2e450865709ed773f57e
HareNishijima <rehare1223@gmail.com>
2024-09-10 23:51:42 +0900

    EnemyAttackに攻撃処理を実装

--- Heli/Assets/Enemy/EnemyAttack.cs
+++ Heli/Assets/Enemy/EnemyAttack.cs
@@ -1,16 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class EnemyBulletCollision : MonoBehaviour
+public class EnemyAttack : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag == "Player")
         {
             col.gameObject.GetComponent<PlayerHP>().Damage(1);
-            Debug.Log("!");
         }
         Destroy(gameObject);
     }
