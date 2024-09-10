2f64c3ef2092fee851629871723ec03cc9c073ca
HareNishijima <rehare1223@gmail.com>
2024-09-10 23:48:27 +0900

    その他EnemyBulletに関する処理を追加

--- Heli/Assets/BulletDestroyWall/BulletDestroyWall.cs
+++ Heli/Assets/BulletDestroyWall/BulletDestroyWall.cs
@@ -4,7 +4,7 @@ public class BulletDestroyWall : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.tag == "Bullet")
+        if (col.gameObject.tag == "Bullet" || col.gameObject.tag == "EnemyBullet")
         {
             Destroy(col.gameObject);
         }
