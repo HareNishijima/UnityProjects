bfc2fc4974ff72e98a89efc1df2f870dda842600
HareNishijima <rehare1223@gmail.com>
2024-09-27 22:24:41 +0900

    プレイヤーがいる位置に発射

--- Heli/Assets/Enemy/Shot/EnemyShot.cs
+++ Heli/Assets/Enemy/Shot/EnemyShot.cs
@@ -9,6 +9,15 @@ public class EnemyShot : MonoBehaviour
 
     public void Shot()
     {
-        Instantiate(enemyBulletObject, transform.position, transform.rotation);
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        float angleZ = GetAngleToTarget(transform, playerObject.transform.position);
+        Instantiate(enemyBulletObject, transform.position, Quaternion.Euler(0f, 0f, angleZ));
+    }
+
+    float GetAngleToTarget(Transform selfTransform, Vector2 targetPosition)
+    {
+        Vector2 diffPosition = targetPosition - (Vector2)selfTransform.position;
+        float angleToTarget = Mathf.Atan2(diffPosition.y, diffPosition.x) * Mathf.Rad2Deg;
+        return angleToTarget;
     }
 }
