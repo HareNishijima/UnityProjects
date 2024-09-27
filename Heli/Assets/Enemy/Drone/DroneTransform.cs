e899e10e4c72ebc4e7ad7fec19e965a172700ff8
HareNishijima <rehare1223@gmail.com>
2024-09-27 21:06:59 +0900

    直進するドローンの作成

---
+++ Heli/Assets/Enemy/Drone/DroneTransform.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneTransform : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    Rigidbody2D rigidbody2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 moveVec = -transform.right * moveSpeed;
+        rigidbody2d.position += moveVec * Time.fixedDeltaTime;
+    }
+}
