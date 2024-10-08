f335ee00942df55abb5bbc8d0778e7eed236c34e
HareNishijima <rehare1223@gmail.com>
2024-10-08 22:10:32 +0900

    AttackController呼び出し

--- 2DAction/Assets/Player/PlayerController.cs
+++ 2DAction/Assets/Player/PlayerController.cs
@@ -5,23 +5,51 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     PlayerTransform playerTransform;
+    PlayerAttack playerAttack;
     Vector2 AxisRawInput;
+
+    enum State { Ready, Attack, AttackReturn };
+    State state;
 
     // Start is called before the first frame update
     void Start()
     {
         playerTransform = GetComponent<PlayerTransform>();
+        playerAttack = GetComponent<PlayerAttack>();
         AxisRawInput = Vector2.zero;
+        state = State.Ready;
     }
 
     // Update is called once per frame
     void Update()
     {
         AxisRawInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (state == State.Ready && Input.GetButtonDown("Fire1"))
+        {
+            state = State.Attack;
+            playerAttack.AttackStart();
+        }
+        else if (state == State.Attack && Input.GetButtonUp("Fire1"))
+        {
+            state = State.AttackReturn;
+        }
     }
 
     void FixedUpdate()
     {
-        playerTransform.Input(AxisRawInput);
+        if (state == State.Ready)
+        {
+            playerTransform.Input(AxisRawInput);
+        }
+        else if (state == State.Attack)
+        {
+            playerAttack.AttackPlay();
+        }
+        else if (state == State.AttackReturn)
+        {
+            playerAttack.AttackEnd();
+        }
+
     }
 }
