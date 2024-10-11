df72136e48c1977e857391d86bdd9c773fbe7c72
HareNishijima <rehare1223@gmail.com>
2024-10-11 19:56:59 +0900

    WIP 攻撃のスクリプト

--- 2DAction/Assets/Player/PlayerAttack.cs
+++ 2DAction/Assets/Player/PlayerAttack.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.Video;
 
 public class PlayerAttack : MonoBehaviour
@@ -9,6 +10,7 @@ public class PlayerAttack : MonoBehaviour
     float length;
     public float deltaLength;
     public float deltaReverseLength;
+    public GameObject weaponObject;
 
     void Start()
     {
@@ -23,21 +25,34 @@ public class PlayerAttack : MonoBehaviour
         Vector2 mosuePosition = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         // 攻撃判定を持つオブジェクトの当たり判定を有効化
+        weaponObject.GetComponent<BoxCollider2D>().enabled = true;
     }
 
-    public void AttackPlay(){
+    public void AttackPlay()
+    {
         // 攻撃キー押下中
 
         // この関数が呼び出されるたびにオブジェクトの長さを伸ばす
-        // length += deltaLength;
+        length += deltaLength;
+        weaponObject.transform.localScale = new Vector3(transform.localScale.x, length, transform.localScale.z);
     }
 
-    public void AttackEnd(){
+    public void AttackEnd()
+    {
         // 攻撃キーを離す
 
         // オブジェクトの長さを縮める
-        // length -= deltaReverseLength;
+        length -= deltaReverseLength;
 
         // オブジェクトの長さが0以下になったら当たり判定を無効化
+        if (length <= 0f)
+        {
+            length = 0f;
+            weaponObject.transform.localScale = new Vector3(transform.localScale.x, 0f, transform.localScale.z);
+            weaponObject.GetComponent<BoxCollider2D>().enabled = false;
+            // todo: ステート変更
+        }
+
+        weaponObject.transform.localScale = new Vector3(transform.localScale.x, length, transform.localScale.z);
     }
 }
