737d5ccdab370dca7cb01da6793968e7e0b46d5d
HareNishijima <rehare1223@gmail.com>
2024-11-03 19:58:07 +0900

    攻撃に関するステートのコードを削除

--- 2DAction/Assets/Player/PlayerState.cs
+++ 2DAction/Assets/Player/PlayerState.cs
@@ -5,64 +5,34 @@ using UnityEngine;
 public class PlayerState : MonoBehaviour
 {
 
-    enum AttackState { Ready, Attack };
-    [SerializeField] AttackState attackstate;
-
     enum JumpState { Ground, Rising, Falling };
     [SerializeField] JumpState jumpState;
 
     // Start is called before the first frame update
     void Start()
     {
-        attackstate = AttackState.Ready;
         jumpState = JumpState.Ground;
     }
-
-    public bool IsReady()
-    {
-        return attackstate == AttackState.Ready;
-    }
-
-    public bool IsAttack()
-    {
-        return attackstate == AttackState.Attack;
-    }
-
     public bool IsGround()
     {
         return jumpState == JumpState.Ground;
     }
-
     public bool IsRising()
     {
         return jumpState == JumpState.Rising;
     }
-
     public bool IsFalling()
     {
         return jumpState == JumpState.Falling;
     }
-
-    public void ToReady()
-    {
-        attackstate = AttackState.Ready;
-    }
-
-    public void ToAttack()
-    {
-        attackstate = AttackState.Attack;
-    }
-
     public void ToGround()
     {
         jumpState = JumpState.Ground;
     }
-
     public void ToRising()
     {
         jumpState = JumpState.Rising;
     }
-
     public void ToFalling()
     {
         jumpState = JumpState.Falling;
