ee16bd8ce4224a659b7860f649ce35a32ef74b4f
HareNishijima <rehare1223@gmail.com>
2024-11-13 21:10:03 +0900

    画面外のブロック削除

--- 2DAction/Assets/BlockDeleter.cs
+++ 2DAction/Assets/BlockDeleter.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BlockDeleter : MonoBehaviour
 {
+    [SerializeField] Tilemap tilemap;
+
     GameObject MainCameraObject;
 
-    void Start(){
+    void Start()
+    {
         MainCameraObject = GameObject.FindWithTag("MainCamera");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 cameraPos = MainCameraObject.transform.position;
+        int y = Mathf.CeilToInt(cameraPos.y);
+
+        for (int x = -19; x < 19; x++)
+        {
+            tilemap.SetTile(new Vector3Int(x, y - 10, 0), null);
+        }
     }
 }
