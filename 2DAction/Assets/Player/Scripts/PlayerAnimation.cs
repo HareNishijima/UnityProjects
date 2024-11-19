c757102134e44041a467d5e277b3f83a8e45d2a3
HareNishijima <rehare1223@gmail.com>
2024-11-19 21:38:09 +0900

    タメ中のアニメーションを実装

--- 2DAction/Assets/Player/Scripts/PlayerAnimation.cs
+++ 2DAction/Assets/Player/Scripts/PlayerAnimation.cs
@@ -28,5 +28,6 @@ public class PlayerAnimation : MonoBehaviour
         animator.SetFloat("YSpeed", jumpVec.y);
 
         animator.SetBool("IsGround", playerState.IsGround());
+        animator.SetBool("IsCharge", playerState.IsCharge());
     }
 }
