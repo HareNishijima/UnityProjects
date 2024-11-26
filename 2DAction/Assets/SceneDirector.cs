22dd898035acd488c5ce4f4ccc7dc262aa00c36a
HareNishijima <rehare1223@gmail.com>
2024-11-26 22:05:29 +0900

    scene director作成

---
+++ 2DAction/Assets/SceneDirector.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneDirector : MonoBehaviour
+{
+    enum GameScene { Ready, Play, GameOver, Clear }
+    [SerializeField] GameScene gameScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameScene = GameScene.Play;
+    }
+}
