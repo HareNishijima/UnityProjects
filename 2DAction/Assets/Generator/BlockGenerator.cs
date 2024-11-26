f0b9a59345d34460197e3f52076aeabeef298814
HareNishijima <rehare1223@gmail.com>
2024-11-26 21:56:25 +0900

    足場の生成ルールを変更

--- 2DAction/Assets/Generator/BlockGenerator.cs
+++ 2DAction/Assets/Generator/BlockGenerator.cs
@@ -8,17 +8,24 @@ public class BlockGenerator : MonoBehaviour
     [SerializeField] Tilemap tilemap;
     [SerializeField] Tile blockTile;
 
+    public int startY;
+    public int EndY;
+
     // Start is called before the first frame update
     void Start()
     {
-        for (int y = 3; y < 30; y += 2)
+        // 等間隔に大きい足場を生成する
+        for (int y = startY; y < EndY; y += 10)
         {
+            int x = Random.Range(-4, 5);
 
-            int x = Random.Range(-15, 15);
-            int diff = Random.Range(-1, 2);
-
-            tilemap.SetTile(new Vector3Int(x - 1, y + diff, 0), blockTile);
-            tilemap.SetTile(new Vector3Int(x, y + diff, 0), blockTile);
+            tilemap.SetTile(new Vector3Int(x - 3, y, 0), blockTile);
+            tilemap.SetTile(new Vector3Int(x - 2, y, 0), blockTile);
+            tilemap.SetTile(new Vector3Int(x - 1, y, 0), blockTile);
+            tilemap.SetTile(new Vector3Int(x, y, 0), blockTile);
+            tilemap.SetTile(new Vector3Int(x + 1, y, 0), blockTile);
+            tilemap.SetTile(new Vector3Int(x + 2, y, 0), blockTile);
+            tilemap.SetTile(new Vector3Int(x + 3, y, 0), blockTile);
         }
 
     }
